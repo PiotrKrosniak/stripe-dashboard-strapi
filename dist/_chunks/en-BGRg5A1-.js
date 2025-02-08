4a71f2b2b7dd061413c718f6bcba0a8ea411c4db
PiotrKrosniak <p.krosniak@gmail.com>
2025-02-08 10:44:58 +0100

    Initial commit3

--- dist/_chunks/en-BGRg5A1-.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
-const en = {
-  "plugin.name": "Stripe Dashboard",
-  "plugin.description": "View Stripe payment statistics"
-};
-exports.default = en;
-//# sourceMappingURL=en-BGRg5A1-.js.map
--- dist/_chunks/en-Bmz9VJQ2.mjs
+++
@@ -1,8 +0,0 @@
-const en = {
-  "plugin.name": "Stripe Dashboard",
-  "plugin.description": "View Stripe payment statistics"
-};
-export {
-  en as default
-};
-//# sourceMappingURL=en-Bmz9VJQ2.mjs.map
